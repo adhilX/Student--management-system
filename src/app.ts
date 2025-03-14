9e2bbdb91724d1607ca496327bd12c74b31051a9
adhil2399 <adhi23996@gmail.com>
2025-03-14 16:09:16 +0530

    new change

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import studentRoutes from './routers/studentRoutes.js'
+import studentRoutes from './routes/studentRoutes.js';
 import path from 'path';
+import { fileURLToPath } from "url";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+// Manually define __dirname for ES modules
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Middleware
 app.use(cors());
 app.use(express.json());
