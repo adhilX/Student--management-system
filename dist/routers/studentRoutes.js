e21b48caf7d305c4fd3f115a0a36b757a2234aad
adhil2399 <adhi23996@gmail.com>
2025-03-14 14:47:11 +0530

    Removed node_modules from Git tracking

--- dist/routers/studentRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { getStudents, createStudent, deleteStudent } from "../controllers/studentsController.js";
-const router = Router();
-router.get('/', getStudents);
-router.post('/', createStudent);
-router.delete('/:id', deleteStudent);
-export default router;
